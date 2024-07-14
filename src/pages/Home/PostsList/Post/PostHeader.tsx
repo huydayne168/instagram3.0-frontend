f2f5bc7e7dc7ae8a92bf73d3441a87392b53726e
huydayne168 <huynguyenviet1608@gmail.com>
2024-07-14 17:00:23 +0700

    update favicon and web title

--- src/pages/Home/PostsList/Post/PostHeader.tsx
+++ src/pages/Home/PostsList/Post/PostHeader.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Avatar from "../../../../components/UI/Avatar/Avatar";
 import calculateTimeAgo from "../../../../utils/calculateTimeAgo";
+import useRedirect from "../../../../hooks/useRedirect";
 
 const PostHeader: React.FC<{
     avatar: string | undefined | null;
@@ -10,12 +11,21 @@ const PostHeader: React.FC<{
     // calculate time:
     const timeAgo = calculateTimeAgo(time);
 
+    const gotoProfilePage = useRedirect().gotoProfilePage;
+
     return (
         <div className="flex items-center pl-1 pb-3">
             <Avatar avatarUrl={avatar} className="w-8 h-8" />
 
             <div className="flex items-center ml-3 mb-[2px] text-sm leading-[18px]">
-                <span className="font-semibold ">{username}</span>
+                <span
+                    className="font-semibold cursor-pointer"
+                    onClick={() => {
+                        gotoProfilePage(username);
+                    }}
+                >
+                    {username}
+                </span>
                 <span className="mx-[4px] text-textGray ">•</span>
                 <span className="text-textGray">{timeAgo}</span>
             </div>
